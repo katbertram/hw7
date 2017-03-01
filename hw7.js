4f65db11c1a2171124454194611702e5078afefd
Kate <katbertram5@gmail.com>
2017-03-01 11:02:57 -0800

    finnished

---
+++ hw7.js
@@ -0,0 +1,52 @@
+var locations = [
+    [
+        "Biblio Cafe",
+       48.463422,-123.309981
+    ],
+    [
+    		"UVIC Bookstore",
+        48.466247,
+        -123.309753
+    ],    
+    [
+        "MacLauren Building",
+        48.462746,
+         -123.313339
+    ],
+]
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: new google.maps.LatLng(48.463152,  -123.311588),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3]),
+        map: map
+      });
+
+    var count = 0;
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+           if (locations[i][1]) {
+            alert("Latitude,Longitude: 48.463449,-123.309662");
+            alert(count++);
+        } else if (locations[i][2]) {
+            alert("Latitude,Longitude: 48.466247,-123.309753" + count);
+            alert(count++);
+        } else {
+            alert("Latitude,Longitude: 48.465173,-123.308397" + count)
+            alert(count++);
+        };
+        }
+      })(marker, i));
+    }
